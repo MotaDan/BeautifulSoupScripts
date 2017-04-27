e3efdb96759a479a7c1db08616f2a527d9a16f4e
Daniel Mota <AndroidKing@gmail.com>
2017-04-27 12:42:24 -0400

    #2: Created csv file for list and open and saved back out to txt file.

--- draughtList.py
+++ draughtList.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 r = requests.get('http://www.publichousefl.com/')
 tphsoup = BeautifulSoup(r.text, 'lxml')
 
 draftList = tphsoup.find(id='pu3025')
 
-with open('draughtList.txt', 'w') as f:
+with open('draughtList.csv', 'w', newline='') as f:
+    fileWriter = csv.writer(f)
+    
+    beers = []
     for beer in draftList.find_all('p'):
-        f.write(beer.string + "\n")
+        beers.append(beer.string)#.strip().lstrip('~-'))
+    
+    fileWriter.writerow(beers)
+    
+with open('draughtList.csv', 'r', newline='') as f:
+    fileReader = csv.reader(f)
+
+    with open('draughtList.txt', 'w') as txtf:
+        for row in fileReader:
+            txtf.write("\n".join(row))
