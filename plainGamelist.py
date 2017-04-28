f8a3458432a8d286b52bce9d38eb9ede72b576c5
Daniel Mota <AndroidKing@gmail.com>
2017-04-27 23:51:53 -0400

    #1: Getting game names from database and putting them in text file. Also removing the ability for the database to have duplicate entries. I didn't not get the ids and sources out of the entries.

--- plainGamelist.py
+++ plainGamelist.py
@@ -18,7 +18,8 @@ players INT,
 path TEXT,
 image TEXT,
 id INT,
-source TEXT);"""
+source TEXT,
+unique (name, desc, rating, releasedate, developer, publisher, genre, players, path, image, id, source));"""
 
 cursor.execute(sql_command)
 
@@ -27,37 +28,33 @@ soup = BeautifulSoup(open("gamelist.xml"), "xml")
 games = soup.find_all('game')
 
 for game in games:
-    namestr = game.find('name').string if game.find('name') is not None else ""
-    descstr = game.desc.string if game.desc is not None else ""
-    ratingstr = game.rating.string if game.rating is not None else ""
-    releasedatestr = game.releasedate.string if game.releasedate is not None else ""
-    developerstr = game.developer.string if game.developer is not None else ""
-    publisherstr = game.publisher.string if game.publisher is not None else ""
-    genrestr = game.genre.string if game.genre is not None else ""
-    playersstr = game.players.string if game.players is not None else ""
-    pathstr = game.path.string if game.path is not None else ""
-    imagestr = game.image.string if game.image is not None else ""
+    namestr = game.find('name').string if game.find('name') is not None and game.find('name').string is not None else ""
+    descstr = game.desc.string if game.desc is not None and game.desc.string is not None else ""
+    ratingstr = game.rating.string if game.rating is not None and game.rating.string is not None else ""
+    releasedatestr = game.releasedate.string if game.releasedate is not None and game.releasedate.string is not None else ""
+    developerstr = game.developer.string if game.developer is not None and game.developer.string is not None else ""
+    publisherstr = game.publisher.string if game.publisher is not None and game.publisher.string is not None else ""
+    genrestr = game.genre.string if game.genre is not None and game.genre.string is not None else ""
+    playersstr = game.players.string if game.players is not None and game.players.string is not None else ""
+    pathstr = game.path.string if game.path is not None and game.path.string is not None else ""
+    imagestr = game.image.string if game.image is not None and game.image.string is not None else ""
     
-    sql_command = """INSERT INTO games (game_number, name, desc, rating, releasedate, developer, publisher, genre, players, path, image, id, source)
+    sql_command = """INSERT OR IGNORE INTO games (game_number, name, desc, rating, releasedate, developer, publisher, genre, players, path, image, id, source)
     VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
     
     cursor.execute(sql_command, (namestr, descstr, ratingstr, releasedatestr, developerstr, publisherstr, genrestr, playersstr, pathstr, imagestr, 0, ""))
 
 connection.commit()
 
-names = soup.find_all('name')
-numGames = len(names)
-
-plainNames = []
-for name in names:
-    plainNames.append(name.string)
-
-plainNames.sort()
+cursor.execute("SELECT name FROM games")
+result = cursor.fetchall()
+result.sort()
 
 with open("plain_gamelist.txt", 'w') as f:
-    f.write(str(numGames) + "\n")
+    cursor.execute("SELECT Count(*) FROM games")
+    f.write(str(cursor.fetchone()[0]) + "\n")
     
-    for name in plainNames:
-        f.write(name + "\n")
+    for name in result:
+        f.write(name[0] + "\n")
         
 connection.close()
